ebbb9a39959827de93e7c0d03b26cf14ae8b80a2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-05-30 12:28:30 +0100

    user commands added

--- 05-SECURITY/users.sql
+++ 05-SECURITY/users.sql
@@ -6,4 +6,29 @@ mysql -h localhost -u root -p autoclub
 mysql -h localhost -u johndoe -p autoclub
 
 -- this will create a new user account in mysql
-CREATE USER 'johndoe'@'%' IDENTIFIED BY 'passcode';
+CREATE USER 'johndoe'@'%' IDENTIFIED BY 'johndoe';
+CREATE USER 'janedoe'@'%' IDENTIFIED BY 'janedoe';
+
+-- this will change the password for johndoe
+ALTER USER 'johndoe'@'%' IDENTIFIED BY 'johndoe';
+
+-- this you login with a new password that you created
+mysql -h localhost -u johndoe -p autoclub
+
+-- this will delete a user account
+DROP USER johndoe;
+
+-- this will grant select priviledges to john doe's account
+GRANT SELECT ON world.* TO 'johndoe'@'%';
+
+-- this will grant access to only one table with access to only 2 columns
+GRANT SELECT (Code, Name) ON `world`.`country` TO 'johndoe'@'%';
+GRANT SELECT (Code, Name) ON `world`.`country` TO 'janedoe'@'%';
+
+SHOW CREATE USER johndoe@localhost;
+
+SHOW GRANTS FOR johndoe@localhost;
+
+SHOW GRANTS;
+
+REVOKE SELECT ON city FROM 'johndoe'@'%';
