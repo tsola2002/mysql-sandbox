1a9bac6848b01b46cb8fe0fd10340e818d74f349
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-05-31 11:53:54 +0100

    added grant permissions

--- 05-SECURITY/users.sql
+++ 05-SECURITY/users.sql
@@ -25,10 +25,52 @@ GRANT SELECT ON world.* TO 'johndoe'@'%';
 GRANT SELECT (Code, Name) ON `world`.`country` TO 'johndoe'@'%';
 GRANT SELECT (Code, Name) ON `world`.`country` TO 'janedoe'@'%';
 
-SHOW CREATE USER johndoe@localhost;
+-- show user details for the user johndoe
+SHOW CREATE USER johndoe;
 
-SHOW GRANTS FOR johndoe@localhost;
+-- show priviledges for a particular user
+SHOW GRANTS FOR johndoe
+
 
 SHOW GRANTS;
 
 REVOKE SELECT ON city FROM 'johndoe'@'%';
+
+USE mysql
+
+SELECT user, host, password FROM user;
+
+CREATE USER joe;
+-- this shows use users that have priviledges attached to them
+SELECT GRANTEE FROM information_schema.USER_PRIVILEGES GROUP BY GRANTEE;
+
+SELECT GRANTEE FROM information_schema.COLUMN_PRIVILEGES GROUP BY GRANTEE;
+
+SELECT GRANTEE FROM information_schema.SCHEMA_PRIVILEGES GROUP BY GRANTEE;
+
+SELECT GRANTEE FROM information_schema.TABLE_PRIVILEGES GROUP BY GRANTEE;
+
+-- this will create neccesary databases
+CREATE SCHEMA IF NOT EXISTS employees;
+CREATE SCHEMA IF NOT EXISTS ebike;
+
+-- this will delete users if they exist already
+DROP USER IF EXISTS 'patrick'@'%';
+DROP USER IF EXISTS 'mike'@'%';
+DROP USER IF EXISTS 'webserver'@'%';
+
+
+--enable_warnings
+CREATE USER 'patrick'@'%' IDENTIFIED BY 'patrick';
+CREATE USER 'mike'@'%' IDENTIFIED BY 'mike';
+CREATE USER 'webserver'@'%' IDENTIFIED BY 'webserver' WITH MAX_USER_CONNECTIONS 300;
+GRANT ALL ON employees.* TO 'patrick'@'%';
+GRANT ALL ON ebike.* TO 'patrick'@'%';
+GRANT ALL ON ebike.* TO 'mike'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ebike.* TO 'webserver'@'%';
+e
+-- this will lock a mysql account
+ALTER USER 'janedoe'@'%' ACCOUNT LOCK;
+
+-- this will unlock an sql account
+ALTER USER 'janedoe'@'%' ACCOUNT UNLOCK;
